e1123725af162c092b28191fe33b0c4a9c912977
Fabian Deneuville <deneuville.fabian@live.fr>
2023-03-02 21:06:29 +0100

    Fixing page layout style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 
 function Layout(){
     return (
-        <div>
+        <div className="layout">
             <Header/>
                 <Outlet/>
             <Footer/>
